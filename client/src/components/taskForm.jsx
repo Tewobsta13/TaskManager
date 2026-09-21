80226a74fe4d9e04c883d2a74b7a7b43cd855e40
Tewobsta <tewobstal@gmail.com>
2026-09-21 09:50:10 +0300

    Connect React frontend to task API

---
+++ client/src/components/taskForm.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+
+function TaskForm() {
+  const [title, setTitle] = useState("");
+
+  async function handleAdd() {
+    const response = await fetch("http://localhost:5000/api/tasks", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        title: title,
+      }),
+    });
+
+    const data = await response.json();
+
+    console.log(data);
+
+    setTitle("");
+  }
+
+  return (
+    <div>
+      <input
+        placeholder="Enter a task..."
+        value={title}
+        onChange={(event) => setTitle(event.target.value)}
+      />
+
+      <button onClick={handleAdd}>Add</button>
+    </div>
+  );
+}
+
+export default TaskForm;
