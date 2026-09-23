3cd87e68a05bd5b0cd1a61243f7e6d9618acb0e7
Tewobsta <tewobstal@gmail.com>
2026-09-23 15:46:09 +0300

    Add task completion update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,10 +3,31 @@ import TaskForm from "./components/TaskForm";
 
 function App() {
   const [tasks, setTasks] = useState([]);
+
   function handleTaskAdded(task) {
     setTasks((previousTasks) => [...previousTasks, task]);
   }
+
   console.log(tasks);
+
+  async function toggleTask(task) {
+    const response = await fetch(
+      `http://localhost:5000/api/tasks/${task._id}`,
+      {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          title: task.title,
+          completed: !task.completed,
+        }),
+      },
+    );
+
+    const updatedTask = await response.json();
+  }
+
   useEffect(() => {
     async function getTasks() {
       const response = await fetch("http://localhost:5000/api/tasks");
@@ -22,9 +43,13 @@ function App() {
   return (
     <div>
       <h1>Task Manager</h1>
+
       <TaskForm onTaskAdded={handleTaskAdded} />
+
       {tasks.map((task) => (
-        <p key={task._id}>{task.title}</p>
+        <p key={task._id} onClick={() => toggleTask(task)}>
+          {task.title}
+        </p>
       ))}
     </div>
   );
